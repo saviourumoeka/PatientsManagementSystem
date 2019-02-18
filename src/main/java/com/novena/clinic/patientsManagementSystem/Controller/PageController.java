714eba7147397bb0ea91d87fa2f6b269ac623bc1
Saviour Umoeka <saviourumoeka@gmail.com>
2019-02-18 22:32:32 +0100

    End of Day 36

--- src/main/java/com/novena/clinic/patientsManagementSystem/Controller/PageController.java
+++ src/main/java/com/novena/clinic/patientsManagementSystem/Controller/PageController.java
@@ -9,6 +9,8 @@ import com.novena.clinic.patientsManagementSystem.Entities.Enums.Gender;
 import com.novena.clinic.patientsManagementSystem.Entities.Enums.Genotype;
 import com.novena.clinic.patientsManagementSystem.Entities.Enums.Level;
 import com.novena.clinic.patientsManagementSystem.Entities.Enums.MaritalStatus;
+import com.novena.clinic.patientsManagementSystem.Entities.Enums.StaffCategory;
+import com.novena.clinic.patientsManagementSystem.Entities.Enums.StaffType;
 
 @Controller
 public class PageController {
@@ -27,13 +29,30 @@ public class PageController {
 	
 	@RequestMapping("/registerPatient")
 	public String registerPatient(ModelMap model) {
+		model.addAttribute("breadcrumb", "Register");
+		model.addAttribute("breadcrumbItem", "Register Student");
 		model.addAttribute("pageTitle", "Register Patient");
 		model.addAttribute("genders", Gender.values());
 		model.addAttribute("levels",Level.values());
 		model.addAttribute("maritalStatus", MaritalStatus.values());
 		model.addAttribute("bloodGroups", BloodGroup.values());
 		model.addAttribute("genotypes", Genotype.values());
-		return "registerstudent";
+		model.addAttribute("student", true);
+		model.addAttribute("link", "/clinic/registerStaff");
+		model.addAttribute("forType", "Staff");
+		return "registerpatient";
 	}
-
+	
+	@RequestMapping("/registerStaff")
+	public String registerStaff(ModelMap model) {
+		model.addAttribute("pageTitle", "Register Patient");
+		model.addAttribute("breadcrumb", "Register");
+		model.addAttribute("breadcrumbItem", "Register Staff");
+		model.addAttribute("types", StaffType.values());
+		model.addAttribute("categories", StaffCategory.values());
+		model.addAttribute("staff", true);
+		model.addAttribute("link", "/clinic/registerPatient");
+		model.addAttribute("forType", "Student");
+		return"registerpatient";
+	}
 }
